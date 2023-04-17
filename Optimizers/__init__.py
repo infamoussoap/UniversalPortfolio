244b0db69eb22d264b317ec72befd404e4dcc545
James Chok <james.chok1000+github@gmail.com>
2023-04-17 09:07:24 +0100

    importing cs-tilde

--- Optimizers/__init__.py
+++ Optimizers/__init__.py
@@ -1,4 +1,7 @@
 from .CauchySimplex import cauchy_simplex
+from .CauchySimplexTilde import cauchy_simplex_tilde
+
 from .EGD import egd
 from .EGDTilde import egd_tilde
+
 from .Newton import newton
